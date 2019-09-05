abb6abe0c990e422c1faf6f6503a3109e244ecf9
Felix <felix@Felixs-MacBook-Pro-3.local>
2019-09-05 11:18:53 -0400

    complete work section

--- src/work/TimelineItem.js
+++ src/work/TimelineItem.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import styles from './Work.module.scss'
+import { Waypoint } from 'react-waypoint';
 
 
-function TimeLineItem({bgTopColor, bgColor, textColor, logo, company, position, date, reverse}) {
+function TimeLineItem({bgTopColor, bgColor, textColor, logo, company, position, date, reverse, points}) {
     const [open, setOpen] = useState(false);
     const [scrolled, setScrolled] = useState(false);
+    const thisElement = React.createRef();
 
     const handleClick = () => {
       if (open) {
@@ -14,28 +16,27 @@ function TimeLineItem({bgTopColor, bgColor, textColor, logo, company, position, 
       }
     }
 
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll)
-    })
-
-    const handleScroll = (event) => {
+    const handleWaypoint = () => {
       if (!scrolled) {
-        console.log(this)
-        // var position = this.current.getBoundingClientRect()
-        // console.log(position)
-  
-        // if(position.top >= 0 && position.bottom <= (window.innerHeight + position.height )) {
-        //   setScrolled(true)
-        // }
+          setScrolled(true)
       }
     }
 
+ 
+    const pointsList = points.map((point) => {
+      return (
+        <p>{point}</p>
+      )
+    })
+
+    const slideInClass = reverse ? styles.scrolledToR : styles.scrolledTo;
     const gradient = `linear-gradient(to bottom, ${bgTopColor} , ${bgColor})`
 
     return (
       <div className={`${styles.timeLineItemContianer} ${reverse ? styles.timeLineItemContianerR : ""}`} ref={thisElement} >
+        <Waypoint onEnter={handleWaypoint} />
         <div className={`${styles.timeLineLeft} ${reverse ? styles.timeLineLeftR : ""}`} > 
-          <div className={`${styles.companyBox} ${reverse ? styles.companyBoxR : ""} ${scrolled ? styles.scrolledTo : ""}`}  style={{background: gradient}} onClick={handleClick}>
+          <div className={`${styles.companyBox} ${reverse ? styles.companyBoxR : ""} ${scrolled ? slideInClass : ""}`}  style={{background: gradient}} onClick={handleClick}>
             <img src={logo} className={styles.logo} /> 
 
               <p className={styles.moreInfo}>more info</p>
@@ -50,10 +51,7 @@ function TimeLineItem({bgTopColor, bgColor, textColor, logo, company, position, 
           <h5>{date}</h5>
           
           <div className={`${styles.infoBox} ${open ? styles.infoBoxOpen : styles.infoBoxClosed}`} >
-            <p>Maintained and developed new features and pages for Snaptravel.com, a large scale and heavily trafficked website using React and Redux</p>
-            <p>Created A/B tests for features which measured user acceptance, impact, and conversion rate</p>
-            <p>Developed the redesign of the home and informational pages using React with server-side rendering</p>
-            <p>Designed and implemented the terms and press release pages.</p>
+            {pointsList}
           </div>
         </div>
       </div>
--- src/work/Work.js
+++ src/work/Work.js
@@ -22,7 +22,12 @@ function Work() {
               logo={Snaptravel} 
               company="SnapTravel"
               position="Front End Web Developer"
-              date="September - December 2018"/>
+              date="September - December 2018"
+              points={[
+                  "Maintained and developed new features and pages for Snaptravel.com, a large scale and heavily trafficked website using React and Redux",
+                  "Created A/B tests for features which measured user acceptance, impact, and conversion rate",
+                  "Built the home and informational pages from scratch using React with server-side rendering",
+                  "Designed and built the terms & conditions and press release pages"]} />
             <TimelineItem 
               bgTopColor='#56A9E6'
               bgColor="#2B70C9"
@@ -31,7 +36,11 @@ function Work() {
               company="American Express"
               position="Software Engineer"
               date="September - December 2017"
-              reverse={true}/>
+              reverse={true}
+              points={[
+                "Designed and developed web prototypes from scratch using React, Node.js and Express",
+                "Designed website layout mock-ups using Adobe Illustrator and Photoshop",
+                "Created methods to target insurance products to card holders based on their purchases, which won 3rd place out of over 200 global teams in American Express internal hackathon"]} />
             <TimelineItem 
               bgTopColor='#8143D0'
               bgColor="#4A20A3"
@@ -39,7 +48,11 @@ function Work() {
               logo={TextNow}
               company="TextNow"
               position="Full-Stack Web Developer"
-              date="January - march 2017"/>
+              date="January - march 2017"
+              points={[
+                "Maintained and developed new features for textnow.com, a large scale and heavily trafficked website with millions of users, using Backbone.js, Node.js and Express",
+                "Implemented storefront design refresh that reduced page bounce rate by 65%",
+                "Took responsibility for quality of assigned projects which includes understanding requirements and impact, creating and updating unit tests, fixing bugs, and performance analysis with A/B testing"]} />
             <TimelineItem 
               bgTopColor='#6AEBFF'
               bgColor="#009ed7"
@@ -48,7 +61,11 @@ function Work() {
               company="Veriday"
               position="Full-Stack Web Developer"
               date="May – September 2016"
-              reverse={true}/>
+              reverse={true}
+              points={[
+                "Worked on enterprise level portal used by bank employees/advisors to generate and monitor both websites and marketing campaigns",
+                "Implemented and maintained back-end solutions: RESTful web service APIs, authentication, and data manipulations using Java and Spring MVC ",
+                "Designed and developed front-end UI, functionality, and API integration with Backbone.js, jQuery, Bootstrap and Less"]} />
             <TimelineItem 
               bgTopColor='#00D98B'
               bgColor="#00b153"
@@ -56,7 +73,9 @@ function Work() {
               logo={TD}
               company="TD Securities"
               position="Quality Assurance Analysis"
-              date="September – December 2014 "/>
+              date="September – December 2014"
+              points={[
+                "Created comprehensive test plans and performed system testing for market risk applications and databases using SQL"]} />
             <TimelineItem 
               bgTopColor='#97FFFA'
               bgColor="#00b5a3"
@@ -65,7 +84,9 @@ function Work() {
               company="Pivotal Labs"
               position="Agile Test Engineer"
               date="January - May 2014" 
-              reverse={true}/>
+              reverse={true}
+              points={[
+                "Implemented test cases for various customer facing mobile applications and websites"]} />
           </div>
         </div>
       </div>
